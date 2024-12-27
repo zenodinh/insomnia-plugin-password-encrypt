56c8e86c49c65d62882c27276a242c2253272ffc
zenodinh <quandinh33@gmail.com>
2024-12-27 12:10:08 +0700

    Update algorithm to PKCS1

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
-const NodeRSA = require('node-rsa');
+const crypto = require('crypto');
 
-function encrypt(publicKeyBase64, salt, message) {
-    let publicKeyPem = `-----BEGIN PUBLIC KEY-----
+
+
+function getPublicKeyPem(publicKeyBase64) {
+    // Public key (in PEM format)
+    const publicKey = `-----BEGIN PUBLIC KEY-----
 ${publicKeyBase64}
 -----END PUBLIC KEY-----`;
-    
-    let encryptor = new NodeRSA();
-    encryptor.importKey(publicKeyPem, 'pkcs8-public-pem');
+    return publicKey;
+}
 
-    let buffer = Buffer.from(encryptor.encrypt(salt+message));
-    return buffer.toString("base64");
+// Function to encrypt password using RSA and public key
+function encryptPassword(publicKeyBase64, salt, password) {
+    // Concatenate salt and password
+    const data = salt + password;
+
+    // Use crypto module to encrypt with the public key
+    const buffer = Buffer.from(data, 'utf-8');
+
+    // Initialize the encryption using the public key
+    const encrypted = crypto.publicEncrypt(
+        {
+            key: getPublicKeyPem(publicKeyBase64),
+            padding: crypto.constants.RSA_PKCS1_PADDING,
+        },
+        buffer
+    );
+
+    // Return the encrypted data as a Base64 string
+    return encrypted.toString('base64');
 }
 
 
@@ -38,6 +57,6 @@ module.exports.templateTags = [{
         },
     ],
     async run(context, publicKeyBase64, salt, message) {
-        return encrypt(publicKeyBase64, salt, message);
+        return encryptPassword(publicKeyBase64, salt, message);
     },
 }]
